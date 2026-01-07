a3a592a1754c9c867cb18d99756984a341900b45
NDEVXLK <ndevslk@gmail.com>
2026-01-07 12:39:10 +0530

    Add files via upload

---
+++ psiphon-client.sh
@@ -0,0 +1,3 @@
+sudo wget -q -O /usr/local/bin/psiphon-client https://raw.githubusercontent.com/ndevxlk/psiphon-client/refs/heads/main/psiphon-client
+sudo chmod +x /usr/local/bin/psiphon-client
+psiphon-client
